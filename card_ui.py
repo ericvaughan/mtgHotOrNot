68ee91b6f63067e2e67aa73d98d8ad116189fcdc
ericlvaughan <ericlvaughan+git@gmail.com>
2019-12-30 22:06:38 -0500

    Goofing with GUI library to learn

--- card_ui.py
+++ card_ui.py
@@ -1,1 +1,13 @@
-# This is the ifc for popping up card images and selecting
+import tkinter
+from PIL import Image, ImageTk
+root=tkinter.Tk()
+image_o = Image.open("images/BlackLotus.png").resize((189, 264), Image.ANTIALIAS)
+card_img1 = ImageTk.PhotoImage(image_o)
+
+image_o = Image.open("images/GoblinChainwhirler.png").resize((189, 264), Image.ANTIALIAS)
+card_img2 = ImageTk.PhotoImage(image_o)
+b1=tkinter.Button(root, image=card_img1)
+b1.pack()
+b2=tkinter.Button(root, image=card_img2)
+b2.pack()
+root.mainloop()
